935566c48d7a6d7feb65f1cecd958d2982479777
Carine Lopes <carine_laraujo@hotmail.com>
2021-05-12 18:01:07 -0300

    Tela de cadastro

---
+++ app/src/main/java/br/com/brq/quizapp/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package br.com.brq.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
